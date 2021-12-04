176512443ed961473a51c7b2702783817d9b5da7
Lockett Pundt <kalifornium@hotmail.com>
2021-12-04 12:20:19 -0500

    feat(AOC-2021): :sparkles: day 4 part one

---
+++ day-four/part-one.js
@@ -0,0 +1,53 @@
+export const giantSquidPartOne = (input) => {
+  const [bingoNumbers, ...boards] = input;
+  const nums = bingoNumbers.split(',');
+  let boardData = formatBoardData(boards);
+  for (const number of nums) {
+    boardData = markNumbers(boardData, number);
+    const winningBoard = boardData.filter(isWinningBoard).flat();
+
+    if (winningBoard.length) {
+      return getScoreOfWinningBoard(winningBoard) * Number(number);
+    }
+  }
+};
+
+export const formatBoardData = (boards) =>
+  boards.map((b) =>
+    b.split('\n').reduce((arr, x) => {
+      return [
+        ...arr,
+        x
+          .split(' ')
+          .filter(Boolean)
+          .map((number) => ({ number, marked: false })),
+      ];
+    }, [])
+  );
+
+export const markNumbers = (boardData, num) =>
+  boardData.map((boards) =>
+    boards.map((board) =>
+      board.map((row) => ({
+        ...row,
+        marked: row.marked || row.number === num,
+      }))
+    )
+  );
+
+export const isWinningBoard = (board) => {
+  if (board.some((row) => row.every((r) => r.marked))) {
+    return true;
+  }
+  return false;
+};
+
+export const getScoreOfWinningBoard = (board) =>
+  board.reduce((sum, row) => {
+    return (sum += row.reduce((rowSum, x) => {
+      if (!x.marked) {
+        rowSum += Number(x.number);
+      }
+      return rowSum;
+    }, 0));
+  }, 0);
