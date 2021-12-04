e42c248b5110eb8d6acaf830c9c8b8aaa0055798
Lockett Pundt <kalifornium@hotmail.com>
2021-12-03 20:20:57 -0500

    test(AOC-2021): :white_check_mark: tests for day three

---
+++ day-three/day-three.test.js
@@ -0,0 +1,21 @@
+import * as fs from 'fs'
+import path from 'path'
+import { binaryDiagnosticPartOne } from './part-one'
+import { lifeSupportRating } from './part-two'
+import { splitDataByNewLine } from '../utils/splitData'
+
+describe('AoC Day 3 Tests', () => {
+  const file = path.join(__dirname, "./", "test-data.txt")
+  const data = fs.readFileSync(file, 'utf-8');
+  const parsedData = splitDataByNewLine(data) 
+
+  it('Part one passes with test data', () => {
+    const result = binaryDiagnosticPartOne(parsedData)
+    expect(result).toEqual(198)
+  })
+
+  it('Part two passes with test data', () => {
+    const result = lifeSupportRating(parsedData)
+    expect(result).toEqual(230)
+  })
+})
