0e31638f9f2c774bd46a053ef7fe72ea9b598653
Lockett Pundt <kalifornium@hotmail.com>
2021-12-05 16:59:41 -0500

    refactor(AOC-2021): :pencil2: fixed typo

--- day-five/part-one.js
+++ day-five/part-one.js
@@ -9,7 +9,7 @@ export const hydrothermalVentsPartOne = (input) => {
     if (starting[0] === ending[0]) {
       const [start, stop] = getStartingPoint(starting[1], ending[1]);
       oceanFloor = updateHorizontal({
-        oceanfloor: oceanFloor,
+        oceanFloor,
         start,
         stop,
         row: starting[0],
@@ -18,7 +18,7 @@ export const hydrothermalVentsPartOne = (input) => {
     if (starting[1] === ending[1]) {
       const [start, stop] = getStartingPoint(starting[0], ending[0]);
       oceanFloor = updateVertical({
-        oceanfloor: oceanFloor,
+        oceanFloor,
         start,
         stop,
         col: starting[1],
@@ -26,7 +26,11 @@ export const hydrothermalVentsPartOne = (input) => {
     }
   }
 
-  const finalScore = oceanFloor.reduce((sum, floor) => {
+  return getFinalScore(oceanFloor);
+};
+
+export const getFinalScore = (oceanFloor) =>
+  oceanFloor.reduce((sum, floor) => {
     sum += floor.reduce((floorSum, x) => {
       if (x.hits > 1) {
         floorSum += 1;
@@ -35,8 +39,6 @@ export const hydrothermalVentsPartOne = (input) => {
     }, 0);
     return sum;
   }, 0);
-  return finalScore;
-};
 
 export const formatData = (input) =>
   input.map((x) =>
@@ -53,23 +55,47 @@ export const createOceanFloor = (length) =>
     }))
   );
 
-export const updateHorizontal = ({ oceanfloor, start, stop, row }) => {
-  oceanfloor[row] = oceanfloor[row].map((spot, i) => {
+export const updateHorizontal = ({ oceanFloor, start, stop, row }) => {
+  oceanFloor[row] = oceanFloor[row].map((spot, i) => {
     if (i >= start && i <= stop) {
       spot.hits += 1;
     }
     return spot;
   });
-  return oceanfloor;
+  return oceanFloor;
 };
 
-export const updateVertical = ({ oceanfloor, start, stop, col }) =>
-  oceanfloor.map((spot, i) => {
+export const updateVertical = ({ oceanFloor, start, stop, col }) =>
+  oceanFloor.map((spot, i) => {
     if (i >= start && i <= stop) {
       spot[col].hits += 1;
     }
     return spot;
   });
+
+export const updateDiagonal = ({ oceanFloor, coordinate }) => {
+  const diagonals = createDiagonals(coordinate);
+  for (const [x, y] of diagonals) {
+    oceanFloor[x] = oceanFloor[x].map((floor, i) => {
+      if (i === y) {
+        floor.hits += 1;
+      }
+      return floor;
+    });
+  }
+  return oceanFloor;
+};
+
+const createDiagonals = (coordinate) => {
+  const [start, stop] = coordinate;
+  const length = Math.abs(start[0] - stop[0]) + 1;
+  return Array.from({ length }).reduce((diagonals, _, i) => {
+    const firstCoordinate = start[0] > stop[0] ? start[0] - i : start[0] + i;
+    const secondCoordinate = start[1] > stop[1] ? start[1] - i : start[1] + i;
+    const nextDiagonal = [firstCoordinate, secondCoordinate];
+    return [...diagonals, nextDiagonal];
+  }, []);
+};
 
 export const getStartingPoint = (numOne, numTwo) => {
   return [numOne, numTwo].sort((a, b) => a - b);
